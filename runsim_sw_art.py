9c3d646af6263080b2cf0361a926f25f80d9f730
Razvan Coca <razvan.coca@gmail.com>
2016-01-28 10:47:08 +0200

    simulation with sw and ART treatment. exposes a run()

---
+++ runsim_sw_art.py
@@ -0,0 +1,35 @@
+"""
+Clone of runsim01. exposes a run method which is the handle by which to enqueue it from outside.
+"""
+from sim01 import run_simulations_mp, onesim
+
+
+# Run model with medium sex worker population 
+from params import ehg_staged03_csw as model_params, phi_ehg
+
+### model globals
+model_params = model_params.copy()
+model_params.update(
+    model_phi = phi_ehg,
+    outfile_pattern = 'eps??sim??.out',
+    n_sim = 100, #the number of replications for each parameter set
+    pop_size = 2 * 10**4, #equal number of males & females
+    )
+
+def run():
+    outfolder = 'temp01'
+    try:
+        import os
+        os.mkdir(outfolder)
+    except OSError:
+        pass
+    model_name = 'test'
+    model_params['outfolder'] = outfolder
+    model_params['model_name'] = model_name
+    run_simulations_mp(model_params)
+
+
+    
+if __name__ == '__main__':
+    run()
+
